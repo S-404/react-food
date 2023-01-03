f29e5a42e7a7b5bce63150669b303f7aed8ca105
s-404 <sudarev.andrey.i@gmail.com>
2023-01-03 22:00:11 +0300

    add ymaps to Location.jsx

--- src/components/UI/location/Location.jsx
+++ src/components/UI/location/Location.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {setLocationVisible} from "../../../store/toolKitRedux/modalsSlice";
+import Modal from "../modal/Modal";
+import MyYMaps from "../map/MyYMaps";
+import './location.scss'
 
 const Location = () => {
+    const locationVisible = useSelector(state => state.modals.locationVisible)
+    const dispatch = useDispatch()
     return (
-        <div>
-            Enter delivery address
+        <div className='location'>
+            <div
+                className='location_selectedLocation'
+                onClick={() => dispatch(setLocationVisible(true))}
+            >
+                select your location
+            </div>
+            <Modal
+                visible={locationVisible}
+                title='Select your location:'
+                setVisible={() => dispatch(setLocationVisible(false))}
+            >
+                <div className='location_locationNav'>
+                    <MyYMaps/>
+                </div>
+            </Modal>
+
+
         </div>
     );
 };
