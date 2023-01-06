69704ec4f2b6793505fa4dc83930c5ecb6e1ff42
s-404 <sudarev.andrey.i@gmail.com>
2023-01-06 19:36:27 +0300

    init hook useMenuCategories.js

---
+++ src/hooks/restaurantMenu/useMenuCategories.js
@@ -0,0 +1,16 @@
+import {useMemo} from "react";
+
+
+export const useMenuCategories = (menu) => {
+    return useMemo(()=>{
+        const resultArray = []
+        if(menu.length){
+            for(let item of menu){
+                if(!resultArray.includes(item.category)){
+                    resultArray.push(item.category)
+                }
+            }
+        }
+        return resultArray.sort()
+    },[menu])
+}
