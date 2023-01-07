6b030ba6741ab74ec3ea0ff4997d8bf669367ecc
s-404 <sudarev.andrey.i@gmail.com>
2023-01-07 15:12:01 +0300

    init ProductCard.jsx

---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './productCard.scss'
+
+const ProductCard = ({product}) => {
+
+    const productDataClickHandler = () => {
+
+    }
+    const buttonClickHandler = () => {
+
+    }
+    return (
+        <div className='productCard'>
+            <div className='productCard__productData'
+                 onClick={productDataClickHandler}
+            >
+                <div className='productCard__imageDiv'>
+                    <img src={product.picture} alt={product.name}/>
+                </div>
+                <div className='productCard__price'>{product.price}</div>
+                <div className='productCard__name'>{product.name}</div>
+                <div className='productCard__weight'>{product.weight}</div>
+            </div>
+            <div className='productCard__buttons'>
+                <div
+                    className='productCard__button'
+                    onClick={buttonClickHandler}
+                >
+                    + Add
+                </div>
+            </div>
+
+
+        </div>
+
+    );
+};
+
+export default ProductCard;
