7b58befaa2a3489c53d33578acf659ea6ce1a52f
s-404 <sudarev.andrey.i@gmail.com>
2023-01-07 14:20:03 +0300

    upd RestaurantMenu.jsx implement MenuContent.jsx

--- src/components/restaurantMenu/RestaurantMenu.jsx
+++ src/components/restaurantMenu/RestaurantMenu.jsx
@@ -1,23 +1,44 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch} from "react-redux";
 import {useFetching} from "../../hooks/useFetching";
+import {setMenu} from "../../store/toolKitRedux/restaurantSlice";
+import MenuCategories from "./categories/MenuCategories";
 import RestaurantService from "../../api/services/RestaurantService";
+import './restaurantMenu.scss'
+import MenuContent from "./content/MenuContent";
+
 
 const RestaurantMenu = () => {
-    const [menu, setMenu] = useState([])
+
+    const dispatch = useDispatch()
     const [fetchMenu, isFetchMenuLoading] = useFetching(
         async () => {
-            const responseData = await  RestaurantService.getRestaurantMenu()
-            setMenu(responseData)
+            const responseData = await RestaurantService.getRestaurantMenu()
+            dispatch(setMenu(responseData))
         }
     )
-    useEffect(()=>{
-        async function fetchData(){await fetchMenu()}
+    useEffect(() => {
+        async function fetchData() {
+            await fetchMenu()
+        }
+
         fetchData()
-    },[])
+    }, [])
+
     if (isFetchMenuLoading) return <div>loading...</div>
+
     return (
-        <div>
-             {menu.length}
+        <div className='RestaurantMenu'>
+            <div className='RestaurantMenu__leftMenu'>
+                <MenuCategories/>
+            </div>
+            <div className='RestaurantMenu__content'>
+                <MenuContent/>
+            </div>
+            <div className='RestaurantMenu__cart'>
+                cart
+            </div>
+
         </div>
     );
 };
