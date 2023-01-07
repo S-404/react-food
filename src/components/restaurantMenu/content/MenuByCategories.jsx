93179b8ef97cf0af87aa01a03a07056c75ff9877
s-404 <sudarev.andrey.i@gmail.com>
2023-01-07 15:13:02 +0300

    upd MenuByCategories.jsx - implement ProductCard.jsx

--- src/components/restaurantMenu/content/MenuByCategories.jsx
+++ src/components/restaurantMenu/content/MenuByCategories.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {useFilteredMenu} from "../../../hooks/restaurantMenu/useMenu";
 import {useSelector} from "react-redux";
-
+import {useFilteredMenu} from "../../../hooks/restaurantMenu/useMenu";
+import ProductCard from "../../productCard/ProductCard";
+import './menuByCategories.scss'
 const MenuByCategories = ({category}) => {
     const menu = useSelector(state => state.restaurant.menu)
     const filteredMenu = useFilteredMenu(menu, 'category', category)
@@ -9,12 +10,16 @@ const MenuByCategories = ({category}) => {
     if (!filteredMenu.length) return null
 
     return (
-        <div>
-            <h3>{category}</h3>
-            <div>
-                {filteredMenu.length}
+        <div className='menuByCategories'>
+            <h3 className='menuByCategories__header'>{category}</h3>
+            <div className='menuByCategories__productList'>
+                {filteredMenu.slice(0,10).map(item=>(
+                    <ProductCard
+                        key={`filteredMenu_productCard_${item.id}`}
+                        product={item}
+                    />
+                ))}
             </div>
-
         </div>
     );
 };
