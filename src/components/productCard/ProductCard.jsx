09744e8b72e91cc3e62b02e6ac6c0eb3017802d9
s-404 <sudarev.andrey.i@gmail.com>
2023-01-08 15:54:48 +0300

    add currency for prices

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './productCard.scss'
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setProductInfoVisible} from "../../store/toolKitRedux/modalsSlice";
 import {setSelectedProduct} from "../../store/toolKitRedux/restaurantSlice";
 import Button from "../UI/buttons/button/Button";
 import ProductCardImg from "./ProductCardImg";
 
 const ProductCard = ({product, isVisible}) => {
+    const storeCurrency = useSelector(state => state.restaurant.data?.currency)
     const dispatch = useDispatch()
     const productDataClickHandler = () => {
         dispatch(setProductInfoVisible(true))
@@ -26,7 +27,9 @@ const ProductCard = ({product, isVisible}) => {
                 <div className='productCard__imageDiv'>
                 <ProductCardImg product={product}/>
                 </div>
-                <div className='productCard__price'>{product.price}</div>
+                <div className='productCard__price'>
+                    {`${product.price} ${storeCurrency}`}
+                </div>
                 <div className='productCard__name'>{product.name}</div>
                 <div className='productCard__weight'>{product.weight}</div>
             </div>
