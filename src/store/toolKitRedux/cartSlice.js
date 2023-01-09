f94d8a31aa08f9e73b9c2fbbefaeb847a2c0a7c6
s-404 <sudarev.andrey.i@gmail.com>
2023-01-09 23:50:08 +0300

    update cartSlice logic

--- src/store/toolKitRedux/cartSlice.js
+++ src/store/toolKitRedux/cartSlice.js
@@ -10,19 +10,35 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            state.cartItems.push(action.payload)
+
+            let itemIndex = state.cartItems.findIndex(item =>  item.id === action.payload.id)
+
+            if (itemIndex === -1) {
+                state.cartItems.push({
+                    ...action.payload,
+                    cartItemQty: 1
+                })
+            } else {
+                state.cartItems[itemIndex].cartItemQty += 1
+            }
+
         },
         removeFromCart: (state, action) => {
             let itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id)
             if (itemIndex !== -1) {
-                state.cartItems.splice(itemIndex, 1)
+                if (state.cartItems[itemIndex].cartItemQty === 1) {
+                    state.cartItems.splice(itemIndex, 1)
+                } else {
+                    state.cartItems[itemIndex].cartItemQty -= 1
+                }
+
             }
         },
-        clearCart:(state)=>{
+        clearCart: (state) => {
             state.cartItems.length = 0
         }
     }
 })
 
 export default cartSlice.reducer
-export const {addToCart,removeFromCart,clearCart} = cartSlice.actions
+export const {addToCart, removeFromCart, clearCart} = cartSlice.actions
