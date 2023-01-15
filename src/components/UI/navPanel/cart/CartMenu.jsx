88afbdba2292d6da5110b32296fa4098ea015d88
s-404 <sudarev.andrey.i@gmail.com>
2023-01-15 11:36:02 +0300

    upd CartMenu.jsx

--- src/components/UI/navPanel/cart/CartMenu.jsx
+++ src/components/UI/navPanel/cart/CartMenu.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import Cart from "../../../cart/Cart";
 
 const CartMenu = () => {
-    const cartItems = useSelector(state => state.cart.cartItems)
     return (
         <div className='navCart__navCartMenu'>
-            {cartItems.map(item=>(
-                <div
-                    key={`cartItem_${item.id}`}
-                    className='navCartMenu__item'
-                >
-                    {item.name}
-                </div>
-            ))}
+            <Cart/>
         </div>
     );
 };
