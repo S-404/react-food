f53eae28694977583901f2f65fae1916d933c3e3
s-404 <sudarev.andrey.i@gmail.com>
2023-01-22 19:04:26 +0300

    remove loader from router

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import DefaultLayout from "../components/defaultLayout/DefaultLayout";
+import {setData} from "../store/toolKitRedux/restaurantSlice";
+import RestaurantService from "../api/services/RestaurantService";
+import {useDispatch} from "react-redux";
+import {useFetching} from "../hooks/useFetching";
+import {useParams} from "react-router-dom";
 
 const Restaurant = () => {
+
+    const dispatch = useDispatch()
+    const params = useParams()
+
+    const [fetchRestaurantMenu] = useFetching(async () => {
+        const list = await RestaurantService.getRestaurantData(params.id)
+        dispatch(setData(list))
+    })
+    useEffect(() => {
+        async function fetchData() {
+            await fetchRestaurantMenu()
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div>
             <DefaultLayout/>
--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import RestaurantsList from "../components/restaurantsList/RestaurantsList";
+import {setList} from "../store/toolKitRedux/restaurantSlice";
+import RestaurantService from "../api/services/RestaurantService";
+import {useDispatch} from "react-redux";
+import {useFetching} from "../hooks/useFetching";
 
 const Restaurants = () => {
+    const dispatch = useDispatch()
+
+    const [fetchRestaurantList, isFetching] = useFetching(async () => {
+        const list = await RestaurantService.getRestaurantList()
+        dispatch(setList(list))
+    })
+    useEffect(() => {
+        async function fetchData() {
+            await fetchRestaurantList()
+        }
+
+        fetchData()
+    }, [])
+
+    if (isFetching) return <div>Loading..</div>
     return (
         <div>
-           <RestaurantsList/>
+            <RestaurantsList/>
         </div>
     );
 };
