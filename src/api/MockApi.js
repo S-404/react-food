bcd6c2fd70c9cd2aa250a0be9ae295e58157e72e
s-404 <sudarev.andrey.i@gmail.com>
2023-01-22 18:38:44 +0300

    upd MockApi.js

--- src/api/MockApi.js
+++ src/api/MockApi.js
@@ -2,26 +2,45 @@ import mockRestaurantMenu from '../store/mockData/mockRestaurantMenu.json'
 import mockRestaurant from '../store/mockData/mockRestaurant.json'
 import mockRestaurantOffer from '../store/mockData/mockRestaurantOffer.json'
 
-const delay = () => new Promise(resolve => setTimeout(resolve, 1000))
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
 export const mockAPI = {
-    getRestaurantList: () => delay().then(() => Promise.resolve(mockRestaurant)),
+    getRestaurantList: () => delay(1000).then(() => Promise.resolve(mockRestaurant)),
 
-    getRestaurantMenu: (restaurantId) => delay().then(() => {
+    getRestaurantMenu: (restaurantId) => delay(1000).then(() => {
         let index = mockRestaurantMenu.findIndex(item => item.restaurantId === restaurantId)
         let result = index !== -1 ? mockRestaurantMenu[index] : {}
         return Promise.resolve(result.categories)
     }),
 
-    getRestaurantData: (id) => delay().then(() => {
-        let index = mockRestaurant.findIndex(item => item.id === id)
+    getRestaurantData: (id) => delay(1000).then(() => {
+        let index = mockRestaurant.findIndex(item => item.id === +id)
         let result = index !== -1 ? mockRestaurant[index] : {}
         return Promise.resolve(result)
     }),
 
-    getRestaurantOffer: (restaurantId) => delay().then(() => {
+    getRestaurantOffer: (restaurantId) => delay(1000).then(() => {
         let index = mockRestaurantOffer.findIndex(item => item.restaurantId === restaurantId)
         let result = index !== -1 ? mockRestaurantOffer[index] : {}
         return Promise.resolve(result)
     }),
+
+    addToCart: (product) => delay(500).then(()=>{
+        //smth billing logic
+        return Promise.resolve(product)
+    }),
+    removeFromCart: (product) => delay(500).then(()=>{
+        //smth billing logic
+        return Promise.resolve(product)
+    }),
+    clearCart: () => delay(500).then(()=>{
+        //smth billing logic
+        let emptyCart = []
+        return Promise.resolve(emptyCart)
+    }),
+    recalculateTotalAmount: ({cartItems, selectedRestaurant})=> delay(500).then(()=>{
+        //smth billing logic
+        let result = {amount: 0, discount: 0, delivery: 0, totalAmount: 0}
+        return Promise.resolve(result)
+    })
 }
