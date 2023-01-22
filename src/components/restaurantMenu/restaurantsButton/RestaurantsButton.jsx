d5b7b6935381784b56e418d9535043154e7ba2ce
s-404 <sudarev.andrey.i@gmail.com>
2023-01-22 18:43:46 +0300

    upd RestaurantsButton.jsx add useNavigate

--- src/components/restaurantMenu/restaurantsButton/RestaurantsButton.jsx
+++ src/components/restaurantMenu/restaurantsButton/RestaurantsButton.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import './restaurantButton.scss'
+import {useNavigate} from "react-router-dom";
+
 const RestaurantsButton = () => {
+    const navigate = useNavigate();
+    const itemClickHandler = () => {
+        navigate(`/r/list`)
+    }
     return (
-        <div className='restaurantsButton'>
+        <div
+            className='restaurantsButton'
+            onClick={itemClickHandler}
+        >
             <ArrowBackIosIcon/>
             {'К списку ресторанов'}
         </div>
