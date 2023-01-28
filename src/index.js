4f3513a8df18d6a12d5a83faa1010cda9d2360fb
S-404 <sudarev.andrey.i@gmail.com>
2023-01-28 15:13:18 +0300

    upd AppRouter.jsx

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store/toolKitRedux';
+import {BrowserRouter as Router} from "react-router-dom";
+import {store} from "./store/toolKitRedux";
+import {Provider} from "react-redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
 );
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Restaurants from "../pages/Restaurants";
 import Restaurant from "../pages/Restaurant";
 
 const AppRouter = () => {
 
-    const router = createBrowserRouter([
-        {
-            path: 'r/list',
-            element: <Restaurants/>,
-        },
-        {
-            path: 'r/menu/:id/',
-            element: <Restaurant/>,
-            errorElement: <div>{'Такой страницы не найдено'}</div>,
-        },
-        {path: 'r/*', element: <Restaurants/>}
-
-    ])
-
-    return <RouterProvider router={router}/>
+    return (
+        <Routes>
+            <Route exact path='/' element={<Restaurants/>}/>
+            <Route path='/r/list' element={<Restaurants/>}/>
+            <Route exact path='/r/menu/:id' element={<Restaurant/>}/>
+        </Routes>
+    )
 
 };
 
