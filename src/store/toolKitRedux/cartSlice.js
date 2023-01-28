3d368387fbd6adf753c349a9b7587ea772b2146f
S-404 <sudarev.andrey.i@gmail.com>
2023-01-28 17:59:10 +0300

    upd cartSlice.js - switch reducers to asyncThunk

--- src/store/toolKitRedux/cartSlice.js
+++ src/store/toolKitRedux/cartSlice.js
@@ -1,29 +1,78 @@
-import {createSlice} from "@reduxjs/toolkit"
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import UserCartService from "../../api/services/UserCartService";
 
 
 const initialState = {
     cartItems: [],
+    status: 'idle',
+    error: {}
 }
+
+export const fetchCartItems = createAsyncThunk(
+    'cart/cartItems',
+    () => UserCartService.getCartItems()
+)
+
+export const addToCart = createAsyncThunk(
+    'cart/addToCart',
+    (product) => UserCartService.addToCart(product)
+)
+
+export const removeFromCart = createAsyncThunk(
+    'cart/removeFromCart',
+    (product) => UserCartService.removeFromCart(product)
+)
+
+export const clearCart = createAsyncThunk(
+    'cart/clearCart',
+    ()=> UserCartService.clearCart()
+)
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
-    reducers: {
-        addToCart: (state, action) => {
-
-            let itemIndex = state.cartItems.findIndex(item =>  item.id === action.payload.id)
+    reducers: {},
+    extraReducers: {
+        [fetchCartItems.pending.type]: (state, action) => {
+            state.status = 'loading'
+            state.cartItems = []
+            state.error = {}
+        },
+        [fetchCartItems.fulfilled.type]: (state, action) => {
+            state.status = 'idle'
+            state.cartItems = action.payload
+            state.error = {}
+        },
+        [fetchCartItems.rejected.type]: (state, action) => {
+            state.status = 'idle'
+            state.cartItems = []
+            state.error = action.payload
+        },
+        [addToCart.pending.type]: (state, action) => {
+            state.status = 'loading'
+            state.error = {}
+        },
+        [addToCart.fulfilled.type]: (state, action) => {
+            let itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id)
 
             if (itemIndex === -1) {
-                state.cartItems.push({
-                    ...action.payload,
-                    cartItemQty: 1
-                })
+                state.cartItems.push({...action.payload, cartItemQty: 1})
             } else {
                 state.cartItems[itemIndex].cartItemQty += 1
             }
 
+            state.status = 'idle'
+            state.error = {}
         },
-        removeFromCart: (state, action) => {
+        [addToCart.rejected.type]: (state, action) => {
+            state.status = 'idle'
+            state.error = action.payload
+        },
+        [removeFromCart.pending.type]: (state, action) => {
+            state.status = 'loading'
+            state.error = {}
+        },
+        [removeFromCart.fulfilled.type]: (state, action) => {
             let itemIndex = state.cartItems.findIndex(item => item.id === action.payload.id)
             if (itemIndex !== -1) {
                 if (state.cartItems[itemIndex].cartItemQty === 1) {
@@ -33,12 +82,27 @@ const cartSlice = createSlice({
                 }
 
             }
+            state.error = {}
+            state.status = 'idle'
         },
-        clearCart: (state) => {
+        [removeFromCart.rejected.type]: (state, action) => {
+            state.status = 'idle'
+            state.error = action.payload
+        },
+        [clearCart.pending.type]: (state, action) => {
+            state.status = 'loading'
+            state.error = {}
+        },
+        [clearCart.fulfilled.type]: (state, action) => {
             state.cartItems.length = 0
+            state.error = {}
+            state.status = 'idle'
+        },
+        [clearCart.rejected.type]: (state, action) => {
+            state.status = 'idle'
+            state.error = action.payload
         }
     }
 })
 
 export default cartSlice.reducer
-export const {addToCart, removeFromCart, clearCart} = cartSlice.actions
