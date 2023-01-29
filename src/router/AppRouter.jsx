7d1e0300e74b59dab93e0956cd4573fda3eef5aa
S-404 <sudarev.andrey.i@gmail.com>
2023-01-29 17:23:24 +0300

    init PurchasePage.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom'
 import Restaurants from "../pages/Restaurants";
 import Restaurant from "../pages/Restaurant";
+import PurchasePage from "../pages/PurchasePage";
 
 const AppRouter = () => {
 
@@ -10,6 +11,8 @@ const AppRouter = () => {
             <Route exact path='/' element={<Restaurants/>}/>
             <Route path='/r/list' element={<Restaurants/>}/>
             <Route exact path='/r/menu/:id' element={<Restaurant/>}/>
+            <Route exact path='/r/purchase' element={<PurchasePage/>}/>
+            <Route path='/*' element={<Restaurants/>}/>
         </Routes>
     )
 
