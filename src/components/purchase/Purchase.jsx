d0226fc61844d2495f0b463b3841798d7b7c018a
S-404 <sudarev.andrey.i@gmail.com>
2023-01-30 09:05:47 +0300

    update Purchase.jsx - add back button

--- src/components/purchase/Purchase.jsx
+++ src/components/purchase/Purchase.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import CreditCardView from "./creditCardView/CreditCardView";
 import CreditCardForm from "./creditCardForm/CreditCardForm";
+import './purchase.scss'
+import Button from "../UI/buttons/button/Button";
+import {useNavigate} from "react-router-dom";
 
 const Purchase = () => {
+    const navigate = useNavigate()
+    const backButtonClickHandler = () => {
+        navigate(-1)
+    }
 
     return (
-        <div>
-            <h1>Purchase</h1>
-            <CreditCardView/>
-            <CreditCardForm/>
+        <div className='purchase'>
+            <Button
+                text={'<Назад'}
+                onClick={backButtonClickHandler}
+            />
+            <h1 className='purchase__header'>{'Оплата'}</h1>
+            <div className='purchase__credit-card'>
+                <CreditCardView/>
+                <CreditCardForm/>
+            </div>
+
         </div>
     );
 };
